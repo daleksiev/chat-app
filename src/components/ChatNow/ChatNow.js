f701cf5dd888aecc31748d9ad757adac816aafc8
daleksiev <denislav.alexiev00@gmail.com>
2022-09-18 21:50:43 +0300

    Create the initial websocket client in react

--- src/components/ChatNow/ChatNow.js
+++ src/components/ChatNow/ChatNow.js
@@ -1,19 +1,34 @@
+import { useEffect, useState } from "react";
 import { Layout } from "antd";
 import { Content } from "antd/lib/layout/layout";
 import { Typography, Input, Button } from 'antd';
 import styles from './ChatNow.module.scss';
-import { useState } from "react";
 
 const { Paragraph } = Typography;
 
 const ChatNow = () => {
-    const [content, setContent] = useState();
-    const [sentContent, setSentContent] = useState([]);
-    const onInput = (e) => setContent(e.target.value);
-    const onSubmit = () => {
-        setSentContent([...sentContent, <p>{content}</p>]);
-        setContent('');
-    }
+    const [name, setName] = useState();
+    const [message, setMessage] = useState();
+    const [sentMessage, setsentMessage] = useState([]);
+
+    const socket = new WebSocket("ws://localhost:5050");
+
+    useEffect(() => {
+        socket.onopen = () => {
+            if (name && message && sentMessage.includes(message)) {
+                const data = { name, message }
+                socket.send(JSON.stringify(data));
+                setMessage('');
+            }
+
+            socket.onerror = (err) => console.log('err: ', err);
+            socket.onmessage = (e) => console.log('data: ', e.data);
+        }
+    }, [sentMessage, socket]);
+
+    const onInputName = (e) => setName(e.target.value);
+    const onInputmessage = (e) => setMessage(e.target.value);
+    const onSubmit = () => setsentMessage([...sentMessage, message]);
     const onKeyPress = (e) => {
         if (e.key === 'Enter') {
             onSubmit()
@@ -23,10 +38,11 @@ const ChatNow = () => {
         <Layout className={styles.chatNowLayout}>
             <Content className={styles.chatNowContent}>
                 <Paragraph className={styles.contentParagraph}>Chat Now Content</Paragraph>
+                <Input onInput={onInputName} value={name} placeholder="Your name" className={styles.nameInput} />
                 <Content className={styles.chatNowComments}>
-                    {sentContent}
+                    {sentMessage.map((singlemessage, index) => <p key={index}>{singlemessage}</p>)}
                 </Content>
-                <Input onInput={onInput} onKeyPress={onKeyPress} value={content} />
+                <Input onInput={onInputmessage} onKeyPress={onKeyPress} value={message} />
                 <div className={styles.buttonWrapper}>
                     <Button onClick={onSubmit}>Send</Button>
                 </div>
